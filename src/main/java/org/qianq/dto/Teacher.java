10d96bef372ea3b902e08f43351595fa16d6ffed
Qian Qian <2362597@edu.vaniercollege.qc.ca>
2023-11-24 18:16:34 -0500

    feat: Add Teacher class
    Signed-off-by: Qian Qian <2362597@edu.vaniercollege.qc.ca>

---
+++ src/main/java/org/qianq/dto/Teacher.java
@@ -0,0 +1,42 @@
+package org.qianq.dto;
+
+/**
+ * Teacher of the school.
+ * @author Qian Qian (2362597)
+ */
+public class Teacher {
+    private static int nextId = 1;
+
+    private String lastName;
+    private String firstName;
+    private Department department;
+    private String id;
+
+
+    /**
+     * Constructor. Initialize the id with nextId and other fields with the arguments.
+     * @param lastName The last name of the teacher.
+     * @param firstName The first name of the teacher.
+     * @param department The department of the teacher.
+     */
+    public Teacher(String lastName, String firstName, Department department) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.department = department;
+        this.id = String.format("T%03d", nextId++);
+    }
+
+    /**
+     * Returns a string that represents this teacher.
+     * @return The string that represents this teacher.
+     */
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", department=" + department +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
